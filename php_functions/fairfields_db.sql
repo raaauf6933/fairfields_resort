b0e4f1d5a6f9dfaa5cd4db4599b3623d22ac721a
raaauf6933 <53444557+raaauf6933@users.noreply.github.com>
2020-08-14 23:03:23 +0800

    mema lang

---
+++ php_functions/fairfields_db.sql
@@ -0,0 +1,400 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 14, 2020 at 04:57 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fairfields_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `additional_type`
+--
+
+CREATE TABLE `additional_type` (
+  `additional_id` int(32) NOT NULL,
+  `additional_type` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `additional_amount` float(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `additional_type`
+--
+
+INSERT INTO `additional_type` (`additional_id`, `additional_type`, `description`, `additional_amount`) VALUES
+(1, 'Extra Bed', 'Extra Bed', 100.00),
+(2, 'Gym', 'Gym', 100.00),
+(3, 'Pool', 'Pool', 200.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `billing`
+--
+
+CREATE TABLE `billing` (
+  `id` int(32) NOT NULL,
+  `billing_id` int(32) NOT NULL,
+  `original_capital` float(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `billing`
+--
+
+INSERT INTO `billing` (`id`, `billing_id`, `original_capital`) VALUES
+(50, 71332385, 4000.00),
+(51, 71332385, 9000.00),
+(52, 72019528, 3000.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guest`
+--
+
+CREATE TABLE `guest` (
+  `guest_id` int(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `contact_number` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `addressline_1` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `zipcode` int(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `guest`
+--
+
+INSERT INTO `guest` (`guest_id`, `first_name`, `last_name`, `gender`, `contact_number`, `email`, `addressline_1`, `city`, `zipcode`) VALUES
+(97131610, 'Rauf', 'Dimaampao', 'Male', '1', '6933rauf@gmail.com', '1', '1', 1),
+(97204524, 'ff', 'ff', 'Male', '1213', 'ff@gmail.com', 'ff', 'ff', 121);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guest_additional`
+--
+
+CREATE TABLE `guest_additional` (
+  `id` int(32) NOT NULL,
+  `reservation_id` int(32) NOT NULL,
+  `additional_id` int(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `guest_additional`
+--
+
+INSERT INTO `guest_additional` (`id`, `reservation_id`, `additional_id`) VALUES
+(2, 59729411, 1),
+(3, 59729411, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `payment_id` int(32) NOT NULL,
+  `billing_id` int(32) NOT NULL,
+  `payed_capital` float(32,2) NOT NULL,
+  `payment_date` datetime(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`payment_id`, `billing_id`, `payed_capital`, `payment_date`) VALUES
+(3, 71332385, 13000.00, '2020-08-12 11:07:41.000000'),
+(4, 72019528, 1500.00, '2020-08-13 18:35:32.000000'),
+(5, 72019528, 1500.00, '2020-08-13 18:57:41.000000'),
+(6, 72019528, 200.00, '2020-08-13 19:32:28.000000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receipt_photo`
+--
+
+CREATE TABLE `receipt_photo` (
+  `receipt_photo_id` int(32) NOT NULL,
+  `reservation_id` int(32) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `upload_date` datetime(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `receipt_photo`
+--
+
+INSERT INTO `receipt_photo` (`receipt_photo_id`, `reservation_id`, `photo`, `upload_date`) VALUES
+(2, 59719758, '5725-IMG_9499.JPG', '2020-08-13 12:43:17.00000'),
+(3, 59719758, '5340-Untitled-1.jpg', '2020-08-13 17:13:00.00000'),
+(4, 59729411, '5129-goku-4k-2020-2n-2880x1800.jpg', '2020-08-13 18:02:33.00000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `reservation_id` int(32) NOT NULL,
+  `guest_id` int(32) NOT NULL,
+  `billing_id` int(32) NOT NULL,
+  `reservation_date` datetime NOT NULL,
+  `booking_reference` varchar(255) NOT NULL,
+  `num_guest` int(32) NOT NULL,
+  `checkin_date` date NOT NULL,
+  `checkout_date` date NOT NULL,
+  `expiration_date` datetime NOT NULL,
+  `reservation_type` int(32) NOT NULL,
+  `status` int(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reservation`
+--
+
+INSERT INTO `reservation` (`reservation_id`, `guest_id`, `billing_id`, `reservation_date`, `booking_reference`, `num_guest`, `checkin_date`, `checkout_date`, `expiration_date`, `reservation_type`, `status`) VALUES
+(59719758, 97131610, 71332385, '2020-08-11 16:13:38', '0928CE', 13, '2020-08-12', '2020-08-13', '2020-08-11 23:59:59', 0, 4),
+(59729411, 97204524, 72019528, '2020-08-12 11:08:49', 'DE8F1F', 3, '2020-08-13', '2020-08-14', '2020-08-12 23:59:59', 0, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `room_id` int(11) NOT NULL,
+  `roomtype_id` int(32) NOT NULL,
+  `room_num` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `rooms`
+--
+
+INSERT INTO `rooms` (`room_id`, `roomtype_id`, `room_num`, `status`) VALUES
+(1, 1, '1B1', 0),
+(2, 1, '1B2', 0),
+(3, 2, 'AC1', 0),
+(4, 2, 'AC2', 0),
+(5, 2, 'AC3', 0),
+(6, 2, 'AC4', 0),
+(7, 3, 'AF1', 0),
+(8, 3, 'AF2', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room_reservation`
+--
+
+CREATE TABLE `room_reservation` (
+  `room_reservation_id` int(32) NOT NULL,
+  `reservation_id` int(32) NOT NULL,
+  `room_id` int(32) NOT NULL,
+  `roomtype_name` varchar(255) NOT NULL,
+  `room_price` float(32,2) NOT NULL,
+  `room_num` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `room_reservation`
+--
+
+INSERT INTO `room_reservation` (`room_reservation_id`, `reservation_id`, `room_id`, `roomtype_name`, `room_price`, `room_num`) VALUES
+(10, 59719758, 1, 'Family Room', 2000.00, '1B1'),
+(11, 59719758, 2, 'Family Room', 2000.00, '1B2'),
+(12, 59719758, 3, 'Vip Room', 3000.00, 'AC1'),
+(13, 59719758, 4, 'Vip Room', 3000.00, 'AC2'),
+(14, 59719758, 5, 'Vip Room', 3000.00, 'AC3'),
+(15, 59729411, 6, 'Vip Room', 3000.00, 'AC4');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room_type`
+--
+
+CREATE TABLE `room_type` (
+  `roomtype_id` int(32) NOT NULL,
+  `roomtype_name` varchar(255) NOT NULL,
+  `roomtype_capacity` int(32) NOT NULL,
+  `roomtype_price` int(32) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `roomtype_photo` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `room_type`
+--
+
+INSERT INTO `room_type` (`roomtype_id`, `roomtype_name`, `roomtype_capacity`, `roomtype_price`, `description`, `roomtype_photo`, `status`) VALUES
+(1, 'Family Room', 2, 2000, 'Good for 3', 'sample room-4221-opa.jpg', 1),
+(2, 'Vip Room', 4, 3000, 'Good for 4', 'sample room-4221-opa.jpg', 1),
+(3, 'Ordinary Room', 6, 2500, 'good for 6', 'sample room-4221-opa.jpg', 1),
+(37, 'sample room', 1, 4, 'sample room', 'sample room-3476-Dawn.jpg', 1),
+(38, 'sample test', 10, 10, 'ffffff', 'sample test-6168-Ali.jpg', 1),
+(39, 'demo', 1, 5000, '1', 'demo-6649-Untitled-1.jpg', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `additional_type`
+--
+ALTER TABLE `additional_type`
+  ADD PRIMARY KEY (`additional_id`);
+
+--
+-- Indexes for table `billing`
+--
+ALTER TABLE `billing`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `guest`
+--
+ALTER TABLE `guest`
+  ADD PRIMARY KEY (`guest_id`);
+
+--
+-- Indexes for table `guest_additional`
+--
+ALTER TABLE `guest_additional`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`payment_id`);
+
+--
+-- Indexes for table `receipt_photo`
+--
+ALTER TABLE `receipt_photo`
+  ADD PRIMARY KEY (`receipt_photo_id`);
+
+--
+-- Indexes for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`reservation_id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`room_id`);
+
+--
+-- Indexes for table `room_reservation`
+--
+ALTER TABLE `room_reservation`
+  ADD PRIMARY KEY (`room_reservation_id`);
+
+--
+-- Indexes for table `room_type`
+--
+ALTER TABLE `room_type`
+  ADD PRIMARY KEY (`roomtype_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `additional_type`
+--
+ALTER TABLE `additional_type`
+  MODIFY `additional_id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `billing`
+--
+ALTER TABLE `billing`
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
+--
+-- AUTO_INCREMENT for table `guest`
+--
+ALTER TABLE `guest`
+  MODIFY `guest_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97204525;
+
+--
+-- AUTO_INCREMENT for table `guest_additional`
+--
+ALTER TABLE `guest_additional`
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `payment_id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `receipt_photo`
+--
+ALTER TABLE `receipt_photo`
+  MODIFY `receipt_photo_id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `reservation_id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59729412;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `room_reservation`
+--
+ALTER TABLE `room_reservation`
+  MODIFY `room_reservation_id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `room_type`
+--
+ALTER TABLE `room_type`
+  MODIFY `roomtype_id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
